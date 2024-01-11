f12b78162ae8be3b88205d782cbbab3fde845f72
2Kelvin <rocketmancodes02@gmail.com>
2024-01-11 15:17:22 +0300

    create a new type alias

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''List of floats annotations'''
+
+type floatList = list[float]
+
+
+def sum_list(input_list: floatList) -> float:
+    '''Add up all the floats in the list'''
+    return sum(input_list)
