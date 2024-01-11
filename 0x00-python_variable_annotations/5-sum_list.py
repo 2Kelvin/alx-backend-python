a28fb329b71326897a68cc4a9853979946b509dc
2Kelvin <rocketmancodes02@gmail.com>
2024-01-11 15:23:57 +0300

    create a new type of list

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 '''List of floats annotations'''
+from typing import List
 
-type floatList = list[float]
 
-
-def sum_list(input_list: floatList) -> float:
+def sum_list(input_list: List[float]) -> float:
     '''Add up all the floats in the list'''
     return sum(input_list)
