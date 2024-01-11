fb48cab6d9fff72acdd7630cf8daff41dcb87c9f
2Kelvin <rocketmancodes02@gmail.com>
2024-01-11 17:26:57 +0300

    use mypy

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
 '''More involved type annotations'''
+from typing import Tuple, List, Any
 
 
-def zoom_array(lst: list, factor: int = 2) -> list:
-    '''fixed all annotaion issues with this function using mypy'''
-    zoomed_in: list = [
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    '''fixed annotation issues using mypy'''
+    zoomed_in: List = [
         item for item in lst
         for i in range(factor)
     ]
     return zoomed_in
 
 
-array = [12, 72, 91]
+array = (12, 72, 91)
 
 zoom_2x = zoom_array(array)
 
