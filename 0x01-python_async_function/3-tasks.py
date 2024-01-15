bd1bc497abbe6d7fa1b732d0f0585318eadd62ff
2Kelvin <rocketmancodes02@gmail.com>
2024-01-15 17:27:23 +0300

    fix task 2 check error by using typing List

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''Tasks'''
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int):
+    '''Returns asyncio.Task'''
+    pass
+
+
+if __name__ == '__main__':
+    task_wait_random()
