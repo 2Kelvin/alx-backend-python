ce772db0eaaaa08058514f636abb5247e88b8853
2Kelvin <rocketmancodes02@gmail.com>
2024-01-15 13:48:57 +0300

    delay the time using sleep()

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -5,9 +5,10 @@ import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    '''returns a random float'''
-    delay = random.uniform(float(0), float(max_delay))
-    return delay
+    '''returns a random delay float'''
+    delayedTime = random.uniform(float(0), float(max_delay))
+    await asyncio.sleep(delayedTime)
+    return delayedTime
 
 
 if __name__ == '__main__':
