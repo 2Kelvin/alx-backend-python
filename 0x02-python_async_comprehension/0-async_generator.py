8c7d45352455e1c6e5700da2fcabdb1046f273ac
2Kelvin <rocketmancodes02@gmail.com>
2024-01-16 12:26:14 +0300

    fix err check on task 0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -8,8 +8,8 @@ from typing import Generator
 async def async_generator() -> Generator[int, float, float]:
     '''generator yields a random number between 0 and 10'''
     for _ in range(10):
-        yield random.uniform(0, 10)
         await asyncio.sleep(1)
+        yield random.uniform(0, 10)
 
 
 if __name__ == '__main__':
---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,2 @@
+#!/usr/bin/env python3
+'''Run time for four parallel comprehensions'''
