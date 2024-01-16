cc62aeb90437bd75a777e6af76f1e3a08fb159bd
2Kelvin <rocketmancodes02@gmail.com>
2024-01-16 12:22:42 +0300

    fix err checks on task 1

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-async_generator = __import__('0-async_generator').async_generator
+'''Async Comprehensions'''
 from typing import List
+async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
