5e8b37b9540127b34f8571aba5381256e777c462
2Kelvin <rocketmancodes02@gmail.com>
2024-01-16 12:35:25 +0300

    correct the function yield and retrun annotation typefix task 0 return and yield type annotation inorder to fix error check 8

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,7 +5,7 @@ import asyncio
 from typing import Generator
 
 
-async def async_generator() -> Generator[int, float, float]:
+async def async_generator() -> Generator[float, None, None]:
     '''generator yields a random number between 0 and 10'''
     for _ in range(10):
         await asyncio.sleep(1)
